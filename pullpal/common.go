94e9034dc2d3a2a82608a35420ef8b7c9b1ffdc9
Moby von Briesen <mobyvb@gmail.com>
2023-05-12 00:53:15 -0400

    Support multiple repositories
    Allow pull pal to be configured with multiple code repositories
    Also make wait duration configurable

--- pullpal/common.go
+++ pullpal/common.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/mobyvb/pull-pal/llm"
@@ -15,40 +17,96 @@ import (
 // IssueNotFound is returned when no issue can be found to generate a prompt for.
 var IssueNotFound = errors.New("no issue found")
 
+type Config struct {
+	WaitDuration     time.Duration
+	LocalRepoPath    string
+	Repos            []string
+	Self             vc.Author
+	ListIssueOptions vc.ListIssueOptions
+	Model            string
+	OpenAIToken      string
+}
+
 // PullPal is the service responsible for:
 //   - Interacting with git server (e.g. reading issues and making PRs on Github)
 //   - Generating LLM prompts
 //   - Parsing LLM responses
 //   - Interacting with LLM (e.g. with GPT via OpenAI API)
 type PullPal struct {
-	ctx              context.Context
-	log              *zap.Logger
-	listIssueOptions vc.ListIssueOptions
+	ctx context.Context
+	log *zap.Logger
+	cfg Config
 
-	ghClient       *vc.GithubClient
-	localGitClient *vc.LocalGitClient
-	openAIClient   *llm.OpenAIClient
+	repos        []pullPalRepo
+	openAIClient *llm.OpenAIClient
+}
+
+type pullPalRepo struct {
+	ctx context.Context
+	log *zap.Logger
+
+	listIssueOptions vc.ListIssueOptions
+	ghClient         *vc.GithubClient
+	localGitClient   *vc.LocalGitClient
+	openAIClient     *llm.OpenAIClient
 }
 
 // NewPullPal creates a new "pull pal service", including setting up local version control and LLM integrations.
-func NewPullPal(ctx context.Context, log *zap.Logger, listIssueOptions vc.ListIssueOptions, self vc.Author, repo vc.Repository, model string, openAIToken string) (*PullPal, error) {
-	ghClient, err := vc.NewGithubClient(ctx, log, self, repo)
-	if err != nil {
-		return nil, err
+func NewPullPal(ctx context.Context, log *zap.Logger, cfg Config) (*PullPal, error) {
+	openAIClient := llm.NewOpenAIClient(log.Named("openaiClient"), cfg.Model, cfg.OpenAIToken)
+
+	ppRepos := []pullPalRepo{}
+	fmt.Println("asdfasfdasfasdfasdf")
+	for _, r := range cfg.Repos {
+		fmt.Println(r)
+		parts := strings.Split(r, "/")
+		if len(parts) < 3 {
+			continue
+		}
+		host := parts[0]
+		owner := parts[1]
+		name := parts[2]
+		fmt.Println(host)
+		fmt.Println(owner)
+		fmt.Println(name)
+		newRepo := vc.Repository{
+			LocalPath:  filepath.Join(cfg.LocalRepoPath, owner, name),
+			HostDomain: host,
+			Name:       name,
+			Owner: vc.Author{
+				Handle: owner,
+			},
+		}
+		ghClient, err := vc.NewGithubClient(ctx, log.Named("ghclient-"+r), cfg.Self, newRepo)
+		if err != nil {
+			return nil, err
+		}
+		localGitClient, err := vc.NewLocalGitClient(log.Named("gitclient-"+r), cfg.Self, newRepo)
+		if err != nil {
+			return nil, err
+		}
+		ppRepos = append(ppRepos, pullPalRepo{
+			ctx: ctx,
+			log: log,
+
+			ghClient:       ghClient,
+			localGitClient: localGitClient,
+			openAIClient:   openAIClient,
+
+			listIssueOptions: cfg.ListIssueOptions,
+		})
 	}
-	localGitClient, err := vc.NewLocalGitClient(log, self, repo)
-	if err != nil {
-		return nil, err
+	if len(ppRepos) == 0 {
+		return nil, errors.New("no repos set up")
 	}
 
 	return &PullPal{
-		ctx:              ctx,
-		log:              log,
-		listIssueOptions: listIssueOptions,
+		ctx: ctx,
+		log: log,
 
-		ghClient:       ghClient,
-		localGitClient: localGitClient,
-		openAIClient:   llm.NewOpenAIClient(log.Named("openaiClient"), model, openAIToken),
+		repos:        ppRepos,
+		openAIClient: openAIClient,
+		cfg:          cfg,
 	}, nil
 }
 
@@ -57,67 +115,78 @@ func (p *PullPal) Run() error {
 	p.log.Info("Starting Pull Pal")
 	// TODO gracefully handle context cancelation
 	for {
-		p.log.Info("checking github issues...")
-		issues, err := p.ghClient.ListOpenIssues(p.listIssueOptions)
-		if err != nil {
-			p.log.Error("error listing issues", zap.Error(err))
-			return err
-		}
-
-		if len(issues) == 0 {
-			p.log.Info("no issues found")
-		} else {
-			p.log.Info("picked issue to process")
-
-			issue := issues[0]
-			err = p.handleIssue(issue)
+		for _, r := range p.repos {
+			err := r.checkIssuesAndComments()
 			if err != nil {
-				// TODO leave comment if error (make configurable)
-				p.log.Error("error handling issue", zap.Error(err))
-				commentText := fmt.Sprintf("I ran into a problem working on this:\n```\n%w\n```", err)
-				err = p.ghClient.CommentOnIssue(issue.Number, commentText)
-				if err != nil {
-					p.log.Error("error commenting on issue with error", zap.Error(err))
-					return err
-				}
-			}
-		}
-
-		p.log.Info("checking pr comments...")
-		comments, err := p.ghClient.ListOpenComments(vc.ListCommentOptions{
-			Handles: p.listIssueOptions.Handles,
-		})
-		if err != nil {
-			p.log.Error("error listing comments", zap.Error(err))
-			return err
-		}
-
-		if len(comments) == 0 {
-			p.log.Info("no comments found")
-		} else {
-			p.log.Info("picked comment to process")
-
-			comment := comments[0]
-			err = p.handleComment(comment)
-			if err != nil {
-				// TODO leave comment if error (make configurable)
-				p.log.Error("error handling comment", zap.Error(err))
-				commentText := fmt.Sprintf("I ran into a problem working on this:\n```\n%w\n```", err)
-				err = p.ghClient.RespondToComment(comment.PRNumber, comment.ID, commentText)
-				if err != nil {
-					p.log.Error("error commenting on thread with error", zap.Error(err))
-					return err
-				}
+				p.log.Error("issue checking repo for issues and comments", zap.Error(err))
 			}
 		}
 
 		// TODO remove sleep
-		p.log.Info("sleeping 30s")
-		time.Sleep(30 * time.Second)
+		p.log.Info("sleeping", zap.Duration("wait duration", p.cfg.WaitDuration))
+		time.Sleep(p.cfg.WaitDuration)
 	}
 }
 
-func (p *PullPal) handleIssue(issue vc.Issue) error {
+// checkIssuesAndComments will attempt to find and solve one issue and one comment, and then return.
+func (p pullPalRepo) checkIssuesAndComments() error {
+	p.log.Info("checking github issues...")
+	issues, err := p.ghClient.ListOpenIssues(p.listIssueOptions)
+	if err != nil {
+		p.log.Error("error listing issues", zap.Error(err))
+		return err
+	}
+
+	if len(issues) == 0 {
+		p.log.Info("no issues found")
+	} else {
+		p.log.Info("picked issue to process")
+
+		issue := issues[0]
+		err = p.handleIssue(issue)
+		if err != nil {
+			// TODO leave comment if error (make configurable)
+			p.log.Error("error handling issue", zap.Error(err))
+			commentText := fmt.Sprintf("I ran into a problem working on this:\n```\n%w\n```", err)
+			err = p.ghClient.CommentOnIssue(issue.Number, commentText)
+			if err != nil {
+				p.log.Error("error commenting on issue with error", zap.Error(err))
+				return err
+			}
+		}
+	}
+
+	p.log.Info("checking pr comments...")
+	comments, err := p.ghClient.ListOpenComments(vc.ListCommentOptions{
+		Handles: p.listIssueOptions.Handles,
+	})
+	if err != nil {
+		p.log.Error("error listing comments", zap.Error(err))
+		return err
+	}
+
+	if len(comments) == 0 {
+		p.log.Info("no comments found")
+	} else {
+		p.log.Info("picked comment to process")
+
+		comment := comments[0]
+		err = p.handleComment(comment)
+		if err != nil {
+			// TODO leave comment if error (make configurable)
+			p.log.Error("error handling comment", zap.Error(err))
+			commentText := fmt.Sprintf("I ran into a problem working on this:\n```\n%w\n```", err)
+			err = p.ghClient.RespondToComment(comment.PRNumber, comment.ID, commentText)
+			if err != nil {
+				p.log.Error("error commenting on thread with error", zap.Error(err))
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (p *pullPalRepo) handleIssue(issue vc.Issue) error {
 	err := p.ghClient.CommentOnIssue(issue.Number, "working on it")
 	if err != nil {
 		p.log.Error("error commenting on issue", zap.Error(err))
@@ -175,7 +244,7 @@ func (p *PullPal) handleIssue(issue vc.Issue) error {
 	return nil
 }
 
-func (p *PullPal) handleComment(comment vc.Comment) error {
+func (p *pullPalRepo) handleComment(comment vc.Comment) error {
 	if comment.Branch == "" {
 		return errors.New("no branch provided in comment")
 	}
